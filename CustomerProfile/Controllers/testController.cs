023205f940a0bcfd9492a9631f559842ca6e94ed
Nirav Desai <d08nirav@hotmail.com>
2018-10-19 01:07:45 +1100

    Polished test APIs

--- CustomerProfile/Controllers/testController.cs
+++ CustomerProfile/Controllers/testController.cs
@@ -10,36 +10,39 @@ namespace CustomerProfile.Controllers
     [ApiController]
     public class testController : ControllerBase
     {
-        // GET api/test
+        /*// GET api/test
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
-        }
+            return new string[] { "Connection Testing: GET method working prefectly" };
+        }*/
 
         // GET api/test/5
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
-            return "value";
+            return "Connection Testing: GET method working prefectly, received value: " + id;
         }
 
         // POST api/test
         [HttpPost]
-        public void Post([FromBody] string value)
+        public ActionResult<string> Post([FromBody] string value)
         {
+            return "Connection Testing: POST method working prefectly, received value from body: " +value;
         }
 
         // PUT api/test/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public ActionResult<string> Put(int id, [FromBody] string value)
         {
+            return "Connection Testing: PUT method working prefectly, received value: "+ id+" from body: " + value;
         }
 
         // DELETE api/test/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public ActionResult<string> Delete(int id)
         {
+            return "Connection Testing: DELETE method working prefectly, received value: " + id;
         }
     }
 }
